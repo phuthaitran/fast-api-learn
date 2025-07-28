8ffc326c607be6e2c99a8efdecd1b15698544eb3
tide <tide@192.168.1.127>
2025-07-28 18:11:39 +0700

    Add functions and comments

--- test.py
+++ test.py
@@ -4,15 +4,23 @@ app = FastAPI()
 
 items = []
 
+# Define root path
 @app.get("/")
 def index():
     return {"name": "Hello world"}
 
+# Create a new item
 @app.post("/items")
 def create_item(item: str):
     items.append(item)
     return items
 
+# List items
+@app.get("/items")
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+# Get item by its ID
 @app.get("/items/{item_id}")
 def get_item(item_id: int):
     if item_id < len(items):
