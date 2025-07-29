b354f56c1fa2bb64156e1149693b02ce31807443
tide <tide@192.168.1.127>
2025-07-29 15:20:37 +0700

    Rename file and update functions

--- student_list.py
+++ student_list.py
@@ -1,47 +1,46 @@
-from fastapi import FastAPI, HTTPException, Path
+from typing import Any, Annotated
+
+from fastapi import FastAPI, HTTPException, Path, Query
 from pydantic import BaseModel
-from typing import Optional
 
-app = FastAPI()
-
-students = {
-    1: {
-        "name": "John",
-        "age": 17,
-        "year": "12A"
-    },
-    2: {
-        "name": "Tim",
-        "age": 15,
-        "year": "10D"
-    },
-}
+app = FastAPI(
+    title="Student Database"
+)
 
 class Student(BaseModel):
     name: str
     age: int
     year: str
     
-class UpdateStudent(BaseModel):
-    name: Optional[str] = None
-    age: Optional[int] = None
-    year: Optional[str] = None
+students = {
+    1: Student(
+        name="John", age=17, year="12A"
+    ),
+    2: Student(
+        name="Tim", age=15, year="10D"
+    )
+}
 
 @app.get("/")
 def index():
-    return {"name": "Hello world"}
+    return {"list of students": students}
 
 # Get student by ID
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int):
+    if student_id not in students:
+        raise HTTPException(status_code=404, detail=f"There is no student with ID {student_id}")
     return students[student_id]
 
 # Get student by name
 @app.get("/get-by-name")
-async def get_student(name: str):
-    for student_id in students:
-        if students[student_id]["name"] == name:
-            return students[student_id]
+def get_student_by_name(name: str) -> dict[str, Any]:
+    def check_name(student: Student) -> bool:
+        return student.name == name
+    
+    selection = [student for student in students.values() if check_name(student)]  # Fixed: pass student instead of name
+    
+    return {"selection": selection}
         
 # Create a Student object
 @app.post("/create-student/{student_id}")
@@ -54,20 +53,26 @@ def create_student(student_id: int, student: Student):
 
 # Update student info
 @app.put("/update-student/{student_id}")
-def update_student(student_id: int, student: UpdateStudent):
+def update_student(
+    student_id: Annotated[int, Path(gt=0)],
+    name: str | None = None,
+    age: Annotated[int | None, Query(gt=0)] = None,
+    year: str | None = None
+):
     if student_id not in students:
-        return {"Error": "Student does not exist"}
+        raise HTTPException(status_code=404, detail=f"Student with id {student_id} does not exist")
+    if all(info is None for info in (name, age, year)):
+        raise HTTPException(status_code=400, detail="No parameters provided for update")
     
-    if student.name != None:
-        students[student_id].name = student.name
+    student = students[student_id]
+    if name is not None:
+        student.name = name
+    if age is not None:
+        student.age = age
+    if year is not None:
+        student.year = year
         
-    if student.age != None:
-        students[student_id].age = student.age
-        
-    if student.year != None:
-        students[student_id].year = student.year    
-        
-    return students[student_id]
+    return {"updated": student}
 
 # Delete a student
 @app.delete("/delete-student/{student_id}")
@@ -75,5 +80,5 @@ def delete_student(student_id: int):
     if student_id not in students:
         return {"Error": "Student does not exist"}
     
-    del students[student_id]
-    return {"Message": "Student deleted sucessfully"}
+    student = students.pop(student_id)
+    return {"deleted": student}
