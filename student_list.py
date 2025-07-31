ce0178d10777e9491cedcf563ad77c030d906247
tide <tide@192.168.1.127>
2025-07-31 13:14:29 +0700

    Separate is_passed to a single body parameter

--- student_list.py
+++ student_list.py
@@ -2,7 +2,7 @@ from typing import Any, Annotated
 from datetime import date
 from enum import Enum
 
-from fastapi import FastAPI, HTTPException, Path, Query
+from fastapi import FastAPI, HTTPException, Path, Query, Body
 from pydantic import BaseModel, Field
 
 app = FastAPI(
@@ -25,18 +25,19 @@ class Mark(BaseModel):
     attendance: Annotated[float, Field(ge=0, le=20)]
     mid_term: Annotated[float, Field(ge=0, le=20)]
     final: Annotated[float, Field(ge=0, le=20)]
-    is_passed: bool
     
 students = [
     {
         "student_id": 1,
         "student": Student(name="Nam", dob=date(2005, 7, 30), group=Major.ICT),
-        "mark": Mark(attendance=20, mid_term=17.5, final=15, is_passed=True)
+        "mark": Mark(attendance=20, mid_term=17.5, final=15),
+        "is_passed": True
     },
     {
         "student_id": 2,
         "student": Student(name="Long", dob=date(2005, 12, 1), group=Major.COMPUTER_SCIENCE),
-        "mark": Mark(attendance=18, mid_term=10, final=10.5, is_passed=True)
+        "mark": Mark(attendance=18, mid_term=10, final=10.5),
+        "is_passed": False
     }
 ]
 
@@ -48,7 +49,7 @@ def index():
 
 # Get student by ID 
 @app.get(
-    "/get-student/{student_id}",
+    "/students/{student_id}",
     responses={
         404: {"description": "Not found"},
     },
@@ -59,65 +60,7 @@ def get_student(student_id: int):
             return student
     raise HTTPException(status_code=404, detail=f"Student with ID {student_id} does not exist")
 
-
-# Get student by name
-@app.get(
-    "/get-by-name",
-    responses={
-        404: {"description": "Not found"},
-    },
-)
-def get_student_by_name(name: Annotated[str | None, Query(description="Enter first name of the student")] = None):
-    if name is None:
-        return {"students": students}
-    
-    matched_students = []
-    for student in students:
-        # Check if the student has 'student' key and if that student has a name that matches
-        if "student" in student and isinstance(student["student"], Student) and student["student"].name.lower() == name.lower():
-            matched_students.append(student)      
-            
-    if not matched_students:
-        raise HTTPException(status_code=404, detail=f"Student with name {name} does not exist")  
-    
-    return {"student": matched_students}
-
-# Get student whether they're passed or not
-@app.get(
-    "/get-by-status",
-    responses={
-        404: {"description": "Not found"},
-    },
-)
-def get_student_by_status(passed: bool):
-    matched_students = []
-    for student in students:
-        if "mark" in student and isinstance(student["mark"], Mark) and student["mark"].is_passed == passed:
-            matched_students.append(student)
-            
-    if not matched_students:
-        raise HTTPException(status_code=404, detail="No students found in this query.")
-    
-    return {"student": matched_students}
-
-# Get student by their group
-@app.get(
-    "/get-by-group",
-    responses={
-        404: {"description": "Not found"},
-    }
-)
-def get_student_by_group(group: Major):
-    matched_students = []
-    for student in students:
-        if "student" in student and isinstance(student["student"], Student) and student["student"].group == group:
-            matched_students.append(student)
-            
-    if not matched_students:
-        raise HTTPException(status_code=404, detail="No students found in this group")
-    
-    return {group: matched_students}
-
+# Get students py parameters
 @app.get(
     "/students",
     responses={
@@ -134,8 +77,8 @@ def query_student_by_parameters(
         if all(
             (
                 name is None or ("student" in student and isinstance(student["student"], Student) and student["student"].name.lower() == name.lower()),
-                passed is None or ("mark" in student and isinstance(student["mark"], Mark) and student["mark"].is_passed == passed),
-                group is None or ("student" in student and isinstance(student["student"], Student) and student["student"].group == group)
+                group is None or ("student" in student and isinstance(student["student"], Student) and student["student"].group == group),
+                passed is None or student["is_passed"] == passed,
             )
         ):
             matched_students.append(student)
@@ -152,16 +95,22 @@ def query_student_by_parameters(
         400: {"description": "Duplicate ID"},
     },
 )
-def create_student(student_id: int, student: Student, mark: Mark):
+def create_student(
+    student_id: int,
+    student: Student,
+    mark: Mark,
+    is_passed: bool = Body(...)
+):
     for existing_student in students:
         if existing_student["student_id"] == student_id:
             raise HTTPException(status_code=400, detail=f"Student with ID {student_id} already exists.")
         
-    new_student = {"student_id": student_id}
-    if student:
-        new_student.update({"student": student})
-    if mark:
-        new_student.update({"mark": mark})
+    new_student = {
+        "student_id": student_id,
+        "student": student,
+        "mark": mark,
+        "is_passed": is_passed
+    }
         
     students.append(new_student)
     return new_student
@@ -169,7 +118,7 @@ def create_student(student_id: int, student: Student, mark: Mark):
 
 # Update student info
 @app.put(
-    "/update-student/{student_id}",
+    "/students/{student_id}",
     responses={
         404: {"description": "Not found"},
         400: {"description": "No arguments specified"},
@@ -178,7 +127,8 @@ def create_student(student_id: int, student: Student, mark: Mark):
 def update_student(
     student_id: Annotated[int, Path(gt=0)],
     student: Student | None = None,
-    mark: Mark | None = None
+    mark: Mark | None = None,
+    is_passed: Annotated[bool | None, Body(...)] = None
 ):
     result = None
     for student_ in students:
@@ -194,6 +144,8 @@ def update_student(
         result["student"] = student
     if mark is not None:
         result["mark"] = mark
+    if is_passed is not None:
+        result["is_passed"] = is_passed
         
     return {"updated": result}
 
